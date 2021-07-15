aac1395efb57eb191a4fab8e445e63fe455f7419
jobsondev1 <jammeloalves@gmail.com>
2021-07-14 23:24:57 -0300

    frontende produto

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -4,10 +4,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Paper from '@material-ui/core/Paper';
+
+import Button from '@material-ui/core/Button';
 import TopBarDrawer from '../../components/TopBarDrawer';
 import Footer from '../../components/Footer'
-
+import Orders  from '../../components/Orders';
 const useStyles = makeStyles((theme) => ({
   root: {display: 'flex', },
     
@@ -15,8 +23,22 @@ const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
   content: {flexGrow: 1, height: '100vh', overflow: 'auto', },
 
-  container: {paddingTop: theme.spacing(4),paddingBottom: theme.spacing(4),},
+  container: {
+    paddingTop: theme.spacing(20),
+    paddingBottom: theme.spacing(4),
+     width: 'center',
+    },
   
+  
+  paper:{
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  formControl:{
+    width: '100%'
+  }
 }));
 
  function Products() {
@@ -30,11 +52,17 @@ const useStyles = makeStyles((theme) => ({
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-
         <Grid container spacing={3}>
-         <h1>Produtos</h1>
-        </Grid>
+        <Grid item xs={12}>
+        <Button variant="contained" color="primary"  disableElevation>
+       Novo Produto
+        </Button>
+        <Paper className={classes.paper}>
+                <Orders />
+              </Paper>
 
+        </Grid>
+        </Grid>
           <Box pt={4}>
             <Footer />
           </Box>
